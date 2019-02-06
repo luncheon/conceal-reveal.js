71cb2be87ff42a5b617fc166e292d7123766f2b9
luncheon <takeuck+github@gmail.com>
2019-02-06 11:38:15 +0900

    fix: correctly restore the original height even if the height property has `important` priority feat: add exports and comments

--- src/conceal-reveal.ts
+++ src/conceal-reveal.ts
@@ -1,87 +1,84 @@
-const CLASS_CONCEALED = 'concealed'
-const CLASS_CONCEALING = 'concealing'
-const CLASS_REVEALING = 'revealing'
+export const CLASS_CONCEALED = 'concealed'
+export const CLASS_CONCEALING = 'concealing'
+export const CLASS_REVEALING = 'revealing'
 
 const DATASET_ORIGINAL_HEIGHT = 'concealRevealOriginalHeight'
 
-function concealingOrConcealed(classList: HTMLElement['classList']): boolean {
-  return classList.contains(CLASS_CONCEALED) || classList.contains(CLASS_CONCEALING)
+/**
+ * Returns whether the element is concealed (including transitioning to be concealed).
+ */
+export function concealed(element: HTMLElement): boolean {
+  return element.classList.contains(CLASS_CONCEALED) || element.classList.contains(CLASS_CONCEALING)
 }
 
+/**
+ * Conceals the element.
+ */
 export function conceal(element: HTMLElement): void {
-  const classList = element.classList
-  if (!concealingOrConcealed(classList)) {
+  if (!concealed(element)) {
+    const classList = element.classList
     if (element.offsetHeight) {
-      const originalHeight = element.style.height
+      if (element.dataset[DATASET_ORIGINAL_HEIGHT] === undefined) {
+        const priority = element.style.getPropertyPriority('height')
+        element.dataset[DATASET_ORIGINAL_HEIGHT] = `${element.style.height || ''}${priority ? '!' + priority : ''}`
+      }
       element.style.setProperty('height', `${getStyleHeight(element)}px`, 'important')
       element.getBoundingClientRect() // reflect height above
       element.addEventListener('transitionend', onTransitionEnd)
       classList.add(CLASS_CONCEALING)
       classList.remove(CLASS_REVEALING)
-      element.style.height = originalHeight
+      element.style.height = ''
     } else {
       classList.add(CLASS_CONCEALED)
     }
   }
 }
 
+/**
+ * Reveals the concealed element.
+ */
 export function reveal(element: HTMLElement): void {
-  const classList = element.classList
-  if (concealingOrConcealed(classList)) {
-    const clone = element.cloneNode(true) as typeof element
-    clone.classList.remove(CLASS_CONCEALING)
-    clone.classList.remove(CLASS_CONCEALED)
-    clone.style.visibility = 'hidden'
-    clone.style.position = 'absolute'
-    clone.style.top = clone.style.left
-                    = 0 as any as string
-    clone.style.setProperty('width', `${getStyleWidth(element)}px`, 'important');
-    element.parentElement!.insertBefore(clone, element)
-    const expandedStyleHeight = getStyleHeight(clone)
-    const expandedOffsetHeight = clone.offsetHeight
-    clone.parentElement!.removeChild(clone)
-    if (expandedOffsetHeight !== element.offsetHeight) {
-      element.addEventListener('transitionend', onTransitionEnd)
-      classList.add(CLASS_REVEALING)
-      element.dataset[DATASET_ORIGINAL_HEIGHT] = element.dataset[DATASET_ORIGINAL_HEIGHT] || element.style.height || ''
-      element.style.setProperty('height', `${expandedStyleHeight}px`, 'important')
-    }
+  if (concealed(element)) {
+    const classList = element.classList
     element.removeAttribute('aria-hidden')
     classList.remove(CLASS_CONCEALING)
     classList.remove(CLASS_CONCEALED)
+    restoreOriginalHeight(element)
+    if (element.offsetHeight) {
+      const expandedStyleHeight = getStyleHeight(element)
+      classList.add(CLASS_CONCEALED)
+      element.style.height = ''
+      element.getBoundingClientRect() // reflect zero-height
+      element.addEventListener('transitionend', onTransitionEnd)
+      classList.add(CLASS_REVEALING)
+      classList.remove(CLASS_CONCEALED)
+      element.style.setProperty('height', `${expandedStyleHeight}px`, 'important')
+    } else {
+      delete element.dataset[DATASET_ORIGINAL_HEIGHT]
+    }
   }
 }
 
+/**
+ * If the element is concealed, reveals the element, otherwise conceals the element.
+ */
 export function toggle(element: HTMLElement): void {
-  (concealingOrConcealed(element.classList) ? reveal : conceal)(element)
+  (concealed(element) ? reveal : conceal)(element)
 }
 
 function onTransitionEnd(this: HTMLElement) {
-  const classList = this.classList
   this.removeEventListener('transitionend', onTransitionEnd)
+
+  const classList = this.classList
   if (classList.contains(CLASS_CONCEALING)) {
     classList.add(CLASS_CONCEALED)
     classList.remove(CLASS_CONCEALING)
     this.setAttribute('aria-hidden', 'true')
-  }
-  classList.remove(CLASS_REVEALING)
-
-  if (this.dataset[DATASET_ORIGINAL_HEIGHT] !== undefined) {
-    this.style.height = this.dataset[DATASET_ORIGINAL_HEIGHT]!
+  } else if (classList.contains(CLASS_REVEALING)) {
+    classList.remove(CLASS_REVEALING)
+    restoreOriginalHeight(this)
     delete this.dataset[DATASET_ORIGINAL_HEIGHT]
   }
-}
-
-function getStyleWidth(element: HTMLElement): number {
-  const style = getComputedStyle(element)
-  return element.offsetWidth - (
-    style.boxSizing === 'border-box'
-      ? 0
-      : parsePx(style.borderLeftWidth!) +
-        parsePx(style.borderRightWidth!) +
-        parsePx(style.paddingLeft!) +
-        parsePx(style.paddingRight!)
-  )
 }
 
 function getStyleHeight(element: HTMLElement): number {
@@ -98,4 +95,9 @@ function getStyleHeight(element: HTMLElement): number {
 
 function parsePx(s: string): number {
   return +s.replace(/px$/, '')
+}
+
+function restoreOriginalHeight(element: HTMLElement) {
+  const originalHeight = (element.dataset[DATASET_ORIGINAL_HEIGHT] || '').split('!')
+  element.style.setProperty('height', originalHeight[0], originalHeight[1])
 }
